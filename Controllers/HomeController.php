246f053285b310c8c44598f0a9b20194c463a2b9
dubar jeremy <jeremy.dubar@epitech.eu>
2021-02-13 13:47:43 +0100

    addPost

--- Controllers/HomeController.php
+++ Controllers/HomeController.php
@@ -1,16 +1,27 @@
 <?php
-require ('Model/member.php');
-require ('Repository/UsersDataRepository.php');
-$UsersDataRepository = new UsersData;
-$getUser = $UsersDataRepository->getUserById($_SESSION["id"]);
-
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
 if(empty($_SESSION['id'])){
   header('Location: index.php?action=');
-} 
+}
 
-    $UserModel = new Member;
-    $UserModel->hydrate($getUser[0]);
-    var_dump($getUser);
-    
-    var_dump($UserModel);
-    include('views/home/home.php');
+require ('Model/member.php');
+require ('Repository/UsersDataRepository.php');
+require ('Repository/TweetsRepository.php');
+$UsersDataRepository = new UsersData;
+$UserModel = new Member;
+$TweetRepository = new TweetsRepository();
+
+// hydratation 
+$getUser = $UsersDataRepository->getUserById($_SESSION["id"]);
+$UserModel->hydrate($getUser[0]);
+
+// postTweet
+$userId = $getUser[0]["ID_user"]; 
+$postTweet = $TweetRepository->postTweet($userId, "ceci est un test25");
+
+
+include('views/home/home.php');
--- Repository/TweetsRepository.php
+++ Repository/TweetsRepository.php
@@ -1,11 +1,16 @@
 <?php 
-require ('Db.php');
+require_once ('Db.php');
 
 class TweetsRepository extends Db {
 
-  public function postTweet()
+  public function postTweet($id_user, $tweet)
   {
-    
+    $sql = "INSERT INTO tweet (ID_user, text, date) VALUES (:ID_user, :tweet, NOW())"; 
+    $query = $this->connect()->prepare($sql);
+    $query->execute([
+      "ID_user" => $id_user,
+      "tweet" => $tweet,
+    ]);
   }
 
 }
