4ba7a0af50e3137521b81d0a0529234406f87e8d
laulauD <laurine.dewaele@epitech.eu>
2021-02-26 12:07:16 +0100

    register css

--- views/home/showTweets.php
+++ views/home/showTweets.php
@@ -30,8 +30,9 @@ var_dump($retweet);
 foreach($tweets as $tweet){ ?>
 
   <div class="tweet-container" style="display: flex; flex-direction: column">
-  <p value="<?=$tweet["textTweet"]?>"><?= $tweet["textTweet"]?></p>
+  
   <span><a href="<?="index.php?action=profil&id_user=". $tweet["UserIDTweet"] .""?>"><?=$tweet["pseudo"]?></a> tweeted :</span>
+  <p value="<?=$tweet["textTweet"]?>"><?= $tweet["textTweet"]?></p>
         <form style="display: inline" action="" method="POST" class="retweet">
         <input type="hidden" name="id_user">
         <button type="submit" class="retweetValue" value="<?= $tweet["IDTweet"]?>" name="id_tweet">Retweet</button>
